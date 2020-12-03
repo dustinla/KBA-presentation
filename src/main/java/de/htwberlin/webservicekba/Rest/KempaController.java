9d82bc2d7c4f8796f3104ffd42545846b73be426
dustinla <dustin.lange94@gmail.com>
2020-12-03 18:18:34 +0100

    add Patch

--- src/main/java/de/htwberlin/webservicekba/Rest/KempaController.java
+++ src/main/java/de/htwberlin/webservicekba/Rest/KempaController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class KempaController {
@@ -43,5 +44,23 @@ public class KempaController {
     public ResponseEntity<Void> saveTodo(@PathVariable("id") Long id) {
         todoRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PatchMapping("/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
+        Optional<Todo> findTodo = todoRepository.findById(id);
+        Todo todo1 = null;
+        if (findTodo.isPresent()) {
+            todo1 = findTodo.get();
+            if (!todo.getBeschreibung().isEmpty()) {
+                todo1.setBeschreibung(todo.getBeschreibung());
+            }
+            if (!todo.getTitel().isEmpty()) {
+                todo1.setTitel(todo.getTitel());
+            }
+
+            todoRepository.save(todo1);
+        }
+        
+        return new ResponseEntity<>(todo1, HttpStatus.OK);
     }
 }
