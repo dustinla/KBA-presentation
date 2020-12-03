cc06c53aa172b460875c1a97d51fc910fefd3f2f
dustinla <dustin.lange94@gmail.com>
2020-12-03 17:15:59 +0100

    add Postgres and Spring data

--- src/main/java/de/htwberlin/webservicekba/Rest/KempaController.java
+++ src/main/java/de/htwberlin/webservicekba/Rest/KempaController.java
@@ -1,16 +1,32 @@
 package de.htwberlin.webservicekba.Rest;
+import de.htwberlin.webservicekba.Model.Todos;
+import de.htwberlin.webservicekba.Repo.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class KempaController {
 
+    @Autowired
+    TodoRepository todoRepository;
 
     @GetMapping("kempa")
     public ResponseEntity<String> kempa(){
 
         return new  ResponseEntity<>("HALLO KEMPA", HttpStatus.OK);
+    }
+
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<Todos>> getAllTodos() {
+        List<Todos> todos = todoRepository.findAll();
+
+        return new ResponseEntity<>(todos, HttpStatus.OK);
+
     }
 }
--- src/main/java/de/htwberlin/webservicekba/WebServiceKbaApplication.java
+++ src/main/java/de/htwberlin/webservicekba/WebServiceKbaApplication.java
@@ -1,13 +1,27 @@
 package de.htwberlin.webservicekba;
 
+import de.htwberlin.webservicekba.Model.Todos;
+import de.htwberlin.webservicekba.Repo.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebServiceKbaApplication {
+public class WebServiceKbaApplication implements CommandLineRunner {
+
+	@Autowired
+	TodoRepository todoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceKbaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Todos todos = Todos.builder().titel("Einkaufen 3.12").beschreibung("Milch, Kaffe, Tee, Toast, Eier").build();
+
+
+		todoRepository.save(todos);
+	}
 }
