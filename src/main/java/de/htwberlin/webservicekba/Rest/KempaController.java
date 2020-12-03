0168a9b30c5b00f45411d87d2591766c5320f3d5
dustinla <dustin.lange94@gmail.com>
2020-12-03 22:22:09 +0100

    Refactor Controller

--- src/main/java/de/htwberlin/webservicekba/Rest/KempaController.java
+++ src/main/java/de/htwberlin/webservicekba/Rest/KempaController.java
@@ -12,12 +12,8 @@ import java.util.Optional;
 public class KempaController {
 
 
-    //TODO Repos eig in eigene Services
-    private final TodoRepository todoRepository;
 
-    public KempaController(TodoRepository todoRepository) {
-        this.todoRepository = todoRepository;
-    }
+
 
     @GetMapping("kempa")
     public ResponseEntity<String> kempa(){
@@ -26,41 +22,5 @@ public class KempaController {
     }
 
 
-    @GetMapping("/todos")
-    public ResponseEntity<List<Todo>> getAllTodos() {
-        List<Todo> todos = todoRepository.findAll();
 
-        return new ResponseEntity<>(todos, HttpStatus.OK);
-
-    }
-
-    @PostMapping("/todos")
-    public ResponseEntity<Todo> saveTodo(@RequestBody Todo todo) {
-        Todo todos = todoRepository.save(todo);
-        return new ResponseEntity<>(todos, HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/todos/{id}")
-    public ResponseEntity<Void> saveTodo(@PathVariable("id") Long id) {
-        todoRepository.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-    @PatchMapping("/todos/{id}")
-    public ResponseEntity<Todo> updateTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
-        Optional<Todo> findTodo = todoRepository.findById(id);
-        Todo todo1 = null;
-        if (findTodo.isPresent()) {
-            todo1 = findTodo.get();
-            if (todo.getBeschreibung() != null) {
-                todo1.setBeschreibung(todo.getBeschreibung());
-            }
-            if (todo.getTitel() != null) {
-                todo1.setTitel(todo.getTitel());
-            }
-
-            todoRepository.save(todo1);
-        }
-        
-        return new ResponseEntity<>(todo1, HttpStatus.OK);
-    }
 }
---
+++ src/main/java/de/htwberlin/webservicekba/Rest/TodosController.java
@@ -0,0 +1,61 @@
+package de.htwberlin.webservicekba.Rest;
+
+import de.htwberlin.webservicekba.Model.Todo;
+import de.htwberlin.webservicekba.Repo.TodoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController("todos")
+public class TodosController {
+
+
+    //TODO Repos eig in eigene Services
+    private final TodoRepository todoRepository;
+
+    public TodosController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        List<Todo> todos = todoRepository.findAll();
+
+        return new ResponseEntity<>(todos, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Todo> saveTodo(@RequestBody Todo todo) {
+        Todo todos = todoRepository.save(todo);
+        return new ResponseEntity<>(todos, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> saveTodo(@PathVariable("id") Long id) {
+        todoRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
+        Optional<Todo> findTodo = todoRepository.findById(id);
+        Todo todo1 = null;
+        if (findTodo.isPresent()) {
+            todo1 = findTodo.get();
+            if (todo.getBeschreibung() != null) {
+                todo1.setBeschreibung(todo.getBeschreibung());
+            }
+            if (todo.getTitel() != null) {
+                todo1.setTitel(todo.getTitel());
+            }
+
+            todoRepository.save(todo1);
+        }
+
+        return new ResponseEntity<>(todo1, HttpStatus.OK);
+    }
+}
