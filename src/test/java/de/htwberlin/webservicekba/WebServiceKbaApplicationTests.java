fa7a7b75d1246eeea88ca9347260c8a49cbf1028
dustinla <dustin.lange94@gmail.com>
2020-12-03 17:22:40 +0100

    disable test

--- src/test/java/de/htwberlin/webservicekba/WebServiceKbaApplicationTests.java
+++ src/test/java/de/htwberlin/webservicekba/WebServiceKbaApplicationTests.java
@@ -3,11 +3,8 @@ package de.htwberlin.webservicekba;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class WebServiceKbaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
