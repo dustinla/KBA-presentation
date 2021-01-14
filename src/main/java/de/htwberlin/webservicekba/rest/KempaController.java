e885a8c96555f3102c8c7324e055718c405daa6e
dustinla <dustin.lange94@gmail.com>
2021-01-14 15:22:17 +0100

    add redirect example

--- src/main/java/de/htwberlin/webservicekba/rest/KempaController.java
+++ src/main/java/de/htwberlin/webservicekba/rest/KempaController.java
@@ -2,17 +2,27 @@ package de.htwberlin.webservicekba.rest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class KempaController {
 
 
     @GetMapping("kempa")
+    @ResponseBody
     public ResponseEntity<String> kempa() {
 
         return new ResponseEntity<>("Hallo Prof. Dr. Kempa, da waren Sie aber neugierig. :)", HttpStatus.OK);
+    }
+
+    @GetMapping("kempa/webseite")
+    public ModelAndView redirectWithUsingRedirectView() {
+        return new ModelAndView("redirect:" + "https://www.htw-berlin.de/hochschule/personen/person/?eid=3888");
+
     }
 
 
