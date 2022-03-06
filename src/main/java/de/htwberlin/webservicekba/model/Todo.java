bdf757133e46dcd163ef92280082f05e4b769aeb
dustinla <dustin.lange94@gmail.com>
2022-03-06 14:56:16 +0100

    add hibernate ehcache 3 example with xml.

--- src/main/java/de/htwberlin/webservicekba/model/Todo.java
+++ src/main/java/de/htwberlin/webservicekba/model/Todo.java
@@ -4,17 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Todo {
 
     @Id
